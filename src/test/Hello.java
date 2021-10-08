777e06fcb1a669367314e983f349e3cc40af6f8c
yunhyun1 <cyh0107@hanmail.net>
2021-10-08 17:12:19 +0900

    프린트 메서드 추가

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("헬로헬로");
+		System.out.println("프린트 다시 써봄");
+		System.out.println("추가추가");
+		
 	}
 
 }
