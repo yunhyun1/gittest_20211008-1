cb1fdd751e328eaf08d96fc46de5a2e9bd8b3a46
yunhyun1 <cyh0107@hanmail.net>
2021-10-12 11:32:30 +0900

    hello-2

---
+++ src/test/Hello-2.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("프린트 다시 써봄");
+		System.out.println("추가추가");
+		
+	}
+
+}
